153d23ed3d3e1164b6770b7e0fe82781bbff068f
Rogi Rocz <47047095+RogiRocz@users.noreply.github.com>
2026-03-27 14:26:21 +0000

    feat: adicionando config do swagger e transformação dos dados

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true
+  }))
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('SGPE - Sistema de Gestão Psicológica')
+    .setDescription('API para gerenciamento de pacientes e prontuários')
+    .setVersion('1.0')
+    .addTag('pacientes')
+    .addTag('prontuarios')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig)
+  SwaggerModule.setup('api/docs', app, document)
+
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`Aplicação rodando em: http://localhost:${process.env.PORT ?? 3000}/api`);
 }
-bootstrap();
+
+bootstrap().then().catch(e => {
+  console.log(`Não foi possível subir servidor. Erro: ${e}`)
+}) 
