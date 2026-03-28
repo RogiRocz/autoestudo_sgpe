e799674c85ba2054e4b2381e2039e6778132e0a9
Rogi Rocz <47047095+RogiRocz@users.noreply.github.com>
2026-03-28 15:46:50 +0000

    fix: simplificando update

--- back/src/modules/prontuario/dto/update-prontuario.dto.ts
+++ back/src/modules/prontuario/dto/update-prontuario.dto.ts
@@ -1,35 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 import { PartialType } from "@nestjs/swagger";
-import { IsDateString, IsEnum, IsInt, IsNotEmpty, IsOptional, IsString, IsUUID } from "class-validator";
-import { LOCAL_SESSAO, PRONTUARIO_STATUS, TIPO_SESSAO } from "orm/generated/prisma/enums";
 import { CreateProntuarioDTO } from "./create-prontuario.dto";
 
-export class UpdateProntuarioDTO extends PartialType(CreateProntuarioDTO){
-    @IsNotEmpty({ message: 'Campo obrigatório' })
-    @IsUUID('4', { message: 'Deve ser um UUID válido' })
-    id_paciente: string;
-
-    @IsOptional()
-    @IsDateString()
-    data_hora: Date;
-
-    @IsOptional()
-    @IsInt()
-    duracao_minutos: number;
-
-    @IsOptional()
-    @IsEnum(TIPO_SESSAO, { message: 'Tipo de sessão inválida' })
-    tipo_sessao: TIPO_SESSAO;
-
-    @IsOptional()
-    @IsEnum(LOCAL_SESSAO, { message: 'Local inválido' })
-    local: LOCAL_SESSAO;
-
-    @IsOptional()
-    @IsEnum(PRONTUARIO_STATUS, { message: 'Status do prontuário do cliente inválido' })
-    status: PRONTUARIO_STATUS;
-
-    @IsOptional()
-    @IsString()
-    observacoes: string;
-}
+export class UpdateProntuarioDTO extends PartialType(CreateProntuarioDTO) { }
