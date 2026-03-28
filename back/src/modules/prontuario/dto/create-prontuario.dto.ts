6341585876845686d1d782ac8cbd73ce475b398e
Rogi Rocz <47047095+RogiRocz@users.noreply.github.com>
2026-03-28 15:46:28 +0000

    feat: criando campo pro aluno e atualizando para string a data_hora

--- back/src/modules/prontuario/dto/create-prontuario.dto.ts
+++ back/src/modules/prontuario/dto/create-prontuario.dto.ts
@@ -1,18 +1,23 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 import { ApiProperty } from "@nestjs/swagger";
 import { IsDateString, IsEnum, IsInt, IsNotEmpty, IsOptional, IsString, IsUUID } from "class-validator";
-import { LOCAL_SESSAO, PRONTUARIO_STATUS, TIPO_SESSAO } from "orm/generated/prisma/enums";
+import { LOCAL_SESSAO, PRONTUARIO_STATUS, TIPO_SESSAO } from "@prisma/client/enums";
 
 export class CreateProntuarioDTO {
     @ApiProperty({ example: 'uuid-do-paciente', description: 'ID único do paciente vinculado' })
     @IsNotEmpty({ message: 'Campo obrigatório' })
     @IsUUID('4', { message: 'Deve ser um UUID válido' })
-    id_paciente: string;
+    paciente_id: string;
+
+    @ApiProperty({ example: 'uuid-do-aluno', description: 'ID único do aluno vinculado' })
+    @IsNotEmpty({ message: 'Campo obrigatório' })
+    @IsUUID('4', { message: 'Deve ser um UUID válido' })
+    aluno_id: string;
 
     @ApiProperty({ example: '2026-03-27T14:00:00Z', description: 'Data e hora da sessão' })
     @IsNotEmpty({ message: 'Campo obrigatório' })
     @IsDateString()
-    data_hora: Date;
+    data_hora: string;
 
     @ApiProperty({ example: 60, required: false, default: 60 })
     @IsOptional()
