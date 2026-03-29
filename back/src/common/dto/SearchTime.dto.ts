e3617200f26a43f229cee1328620c6b6c4fd3221
Rogi Rocz <47047095+RogiRocz@users.noreply.github.com>
2026-03-29 23:09:22 +0000

    fix: ajustando lógica para OwnerData DTO

--- back/src/common/dto/SearchTime.dto.ts
+++ back/src/common/dto/SearchTime.dto.ts
@@ -1,20 +1,9 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsDateString, IsEnum, IsOptional, IsUUID } from "class-validator";
+import { IsDateString, IsOptional } from "class-validator";
+import { OwnerDataDTO } from "./OwnerData.dto";
 
-export enum TipoDono {
-    PACIENTE = 'paciente_id',
-    ALUNO = 'aluno_id'
-}
 
-export class SearchTimeDTO {
-    @ApiProperty({ enum: TipoDono, description: 'Tipo de entidade dona do prontuário' })
-    @IsEnum(TipoDono, { message: 'Tipo de dono inválido' })
-    tipoDono: TipoDono;
-
-    @ApiProperty({ description: 'UUID do Paciente ou Aluno' })
-    @IsUUID()
-    donoId: string;
-
+export class SearchTimeDTO extends OwnerDataDTO {
     @ApiProperty({ example: '2023-01-01T00:00:00.000Z', description: 'Data de início do intervalo' })
     @IsOptional()
     @IsDateString()
