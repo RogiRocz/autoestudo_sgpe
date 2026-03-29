e3941e43bb7a4f58df770a351149c0cdb3af2e6f
Rogi Rocz <47047095+RogiRocz@users.noreply.github.com>
2026-03-29 23:23:12 +0000

    feat: simplificando funções, aplicando DTOs e criando novos finds

--- back/src/modules/prontuario/prontuario.service.ts
+++ back/src/modules/prontuario/prontuario.service.ts
@@ -2,25 +2,24 @@ import { PrismaService } from "src/common/Prisma/prisma.service";
 import { CreateProntuarioDTO } from "./dto/create-prontuario.dto";
 import { Prontuario } from "./entites/prontuario.entity";
 import { BadRequestException, ConflictException, Injectable, NotFoundException } from "@nestjs/common";
-import { Prisma } from "@prisma/client";
+import { LOCAL_SESSAO, Prisma, PRONTUARIO_STATUS } from "@prisma/client";
 import { UpdateProntuarioDTO } from "./dto/update-prontuario.dto";
+import { QueryParamsDTO } from "src/common/dto/QueryParams.dto";
+import { getPaginationPrisma } from "src/common/utils/PrismaQuery.helper";
+import { PacienteService } from "../paciente/paciente.service";
+import { SearchTimeDTO } from "src/common/dto/SearchTime.dto";
+import { AlunoService } from "../aluno/aluno.service";
+import { OwnerDataDTO } from "src/common/dto/OwnerData.dto";
 
 @Injectable()
 export class ProntuarioService {
-    constructor(private prisma: PrismaService) { }
+    constructor(private prisma: PrismaService,
+        private pacienteService: PacienteService,
+        private alunoService: AlunoService) { }
 
     async create(dadosProntuario: CreateProntuarioDTO): Promise<Prontuario> {
         const novoProntuario = await this.prisma.prontuario.create({
-            data: {
-                paciente_id: dadosProntuario.paciente_id,
-                aluno_id: dadosProntuario.aluno_id,
-                data_hora: dadosProntuario.data_hora,
-                duracao_minutos: dadosProntuario.duracao_minutos,
-                tipo_sessao: dadosProntuario.tipo_sessao,
-                local: dadosProntuario.local,
-                status: dadosProntuario.status,
-                observacoes: dadosProntuario.observacoes
-            },
+            data: dadosProntuario,
             include: {
                 aluno: true,
                 paciente: true
@@ -30,20 +29,92 @@ export class ProntuarioService {
         return novoProntuario
     }
 
-    async findAllProntuarios(size: number, page: number): Promise<Prontuario[]> {
-        const skip = size * (page - 1)
+    async findAllProntuarios(params: QueryParamsDTO): Promise<Prontuario[]> {
+        const skip = params.size * (params.page - 1)
         const total_records = await this.prisma.prontuario.count()
 
         if (skip >= total_records) {
             throw new BadRequestException('Valor na paginação: O valor recebido está inválido')
         }
 
+        const query = await this.prisma.prontuario.findMany(
+            getPaginationPrisma(params)
+        )
+
+        return query
+    }
+
+    async findByPaciente(id_paciente: string, params: QueryParamsDTO): Promise<Prontuario[]> {
+        const paciente = await this.pacienteService.findById(id_paciente)
+
         const query = await this.prisma.prontuario.findMany({
-            skip: skip,
-            take: size,
-            orderBy: {
-                uuid: 'asc'
-            }
+            where: {
+                paciente_id: paciente.uuid
+            },
+            ...getPaginationPrisma(params)
+        })
+
+        return query
+    }
+
+    async findByAluno(id_aluno: string, params: QueryParamsDTO): Promise<Prontuario[]> {
+        const aluno = await this.alunoService.findById(id_aluno)
+
+        const query = await this.prisma.prontuario.findMany({
+            where: {
+                aluno_id: aluno.uuid
+            },
+            ...getPaginationPrisma(params)
+        })
+
+        return query
+    }
+
+    async findByLocal(local: LOCAL_SESSAO, params: QueryParamsDTO, owner?: OwnerDataDTO) {
+        const whereClause: Prisma.prontuarioWhereInput = { local }
+
+        if (owner?.tipoDono && owner?.donoId) {
+            whereClause[owner?.tipoDono as string] = owner?.donoId
+        }
+
+        const query = await this.prisma.prontuario.findMany({
+            where: whereClause,
+            ...getPaginationPrisma(params)
+        })
+
+        return query
+    }
+
+    async findByIntervaloTempo(search: SearchTimeDTO, params: QueryParamsDTO): Promise<Prontuario[]> {
+        const whereClause: Prisma.prontuarioWhereInput = {}
+
+        if (search.tipoDono && search.donoId) {
+            whereClause[search.tipoDono as string] = search.donoId
+        }
+
+        whereClause.data_hora = {
+            gte: search.startDate ? new Date(search.startDate) : undefined,
+            lte: search.endDate ? new Date(search.endDate) : undefined
+        }
+
+        const query = await this.prisma.prontuario.findMany({
+            where: whereClause,
+            ...getPaginationPrisma(params)
+        })
+
+        return query
+    }
+
+    async findByStatus(status: PRONTUARIO_STATUS, params: QueryParamsDTO, owner?: OwnerDataDTO): Promise<Prontuario[]> {
+        const whereClause: Prisma.prontuarioWhereInput = { status }
+
+        if (owner?.tipoDono && owner?.donoId) {
+            whereClause[owner?.tipoDono as string] = owner?.donoId
+        }
+
+        const query = await this.prisma.prontuario.findMany({
+            where: whereClause,
+            ...getPaginationPrisma(params)
         })
 
         return query
@@ -77,7 +148,7 @@ export class ProntuarioService {
                 throw new ConflictException('Falha ao atualizar: O prontuário não pode ser mudado de paciente')
             }
 
-            if(prontuarioAntigo.aluno_id != dadosNovos.aluno_id){
+            if (prontuarioAntigo.aluno_id != dadosNovos.aluno_id) {
                 throw new ConflictException('Falha ao atualizar: O prontuário não pode ser mudado de aluno')
             }
 
@@ -103,6 +174,9 @@ export class ProntuarioService {
             const prontuarioDeletado = await this.prisma.prontuario.delete({
                 where: { uuid: id },
                 select: {
+                    uuid: true,
+                    data_hora: true,
+                    aluno_id: true,
                     paciente_id: true
                 }
             })
