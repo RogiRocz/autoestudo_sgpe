42f20ca3880580c81dd23cb4fe87d794860c09a6
Rogi Rocz <47047095+RogiRocz@users.noreply.github.com>
2026-03-31 21:16:59 +0000

    fix: importação dos modulos necessários

--- back/src/modules/prontuario/prontuario.module.ts
+++ back/src/modules/prontuario/prontuario.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
 import { ProntuarioService } from "./prontuario.service";
 import { ProntuarioController } from "./prontuario.controller";
+import { PacienteModule } from "../paciente/paciente.module";
+import { AlunoModule } from "../aluno/aluno.module";
 
 @Module({
+    imports: [PacienteModule, AlunoModule],
     providers: [ProntuarioService],
     controllers: [ProntuarioController],
     exports: [ProntuarioService]
