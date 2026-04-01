29b07edfeebffb09014e09f5603566967ccc481b
Rogi Rocz <47047095+RogiRocz@users.noreply.github.com>
2026-04-01 23:07:34 +0000

    feat: DTO do payload das requisições e respostas

---
+++ back/src/common/dto/UserPayload.dto.ts
@@ -0,0 +1,10 @@
+import { JwtPayload } from "jsonwebtoken";
+import { UserType } from "../interfaces/IAuth.interface";
+
+
+export class UserPayload implements JwtPayload {
+    sub: string
+    type: UserType
+    iat?: number
+    exp?: number
+}
