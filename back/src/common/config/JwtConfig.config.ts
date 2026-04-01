8eb3453f2197216ff0b5804aaaea055a45e93ccb
Rogi Rocz <47047095+RogiRocz@users.noreply.github.com>
2026-04-01 23:07:15 +0000

    feat: função que configura o JwtModule

---
+++ back/src/common/config/JwtConfig.config.ts
@@ -0,0 +1,15 @@
+import { ConfigService } from '@nestjs/config'
+import { JwtModuleAsyncOptions } from '@nestjs/jwt'
+
+export const jwtConfig: JwtModuleAsyncOptions = {
+    inject: [ConfigService],
+    useFactory: (configService: ConfigService) => {
+        const expiresInMinutes = parseInt(configService.get<string>('JWT_SECRET_EXP_MIN', '60'), 10)
+        return {
+            secret: configService.get<string>('JWT_SECRET'),
+            signOptions: {
+                expiresIn: `${expiresInMinutes} m`,
+            },
+        }
+    },
+}
