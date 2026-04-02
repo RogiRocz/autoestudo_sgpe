5928868cb9de4025c18693775d39fd9c9947ff9f
Rogi Rocz <47047095+RogiRocz@users.noreply.github.com>
2026-04-02 22:02:21 +0000

    feat: criação do decorador para a paginação

---
+++ back/src/common/decorator/paginated.decorator.ts
@@ -0,0 +1,26 @@
+import { Type, applyDecorators } from "@nestjs/common";
+import { ApiExtraModels, ApiOkResponse, getSchemaPath } from "@nestjs/swagger";
+import { PaginatedResponse } from "../dto/PaginatedResponse.dto";
+
+export const ApiPaginatedResponse = <TModel extends Type<any>>(model: TModel) => {
+    return applyDecorators(
+        ApiExtraModels(PaginatedResponse, model), // Registra os modelos no Swagger
+        ApiOkResponse({
+            description: `Lista paginada de ${model.name}`,
+            schema: {
+                title: `Modelo de paginação de ${model.name}`,
+                allOf: [
+                    { $ref: getSchemaPath(PaginatedResponse) },
+                    {
+                        properties: {
+                            data: {
+                                type: 'array',
+                                items: { $ref: getSchemaPath(model) },
+                            },
+                        },
+                    },
+                ],
+            },
+        }),
+    );
+};
