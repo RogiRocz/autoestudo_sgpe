c4bcb26fb67dbf83ac9c25f43703be078ffe5631
Rogi Rocz <47047095+RogiRocz@users.noreply.github.com>
2026-04-02 15:18:39 +0000

    feat: usando os modulos das entidades e controller

--- back/src/modules/auth/auth.module.ts
+++ back/src/modules/auth/auth.module.ts
@@ -3,9 +3,13 @@ import { JwtModule } from "@nestjs/jwt";
 import { jwtConfig } from "../../common/config/JwtConfig.config";
 import { AuthService } from "./auth.service";
 import { HashHelper } from "../../common/utils/Hashing.helper";
+import { AuthController } from "./auth.controller";
+import { PacienteModule } from "../paciente/paciente.module";
+import { AlunoModule } from "../aluno/aluno.module";
 
 @Module({
-    imports: [JwtModule.registerAsync(jwtConfig)],
+    imports: [JwtModule.registerAsync(jwtConfig), PacienteModule, AlunoModule],
+    controllers: [AuthController],
     providers: [AuthService, HashHelper],
     exports: [AuthService, HashHelper]
 })
