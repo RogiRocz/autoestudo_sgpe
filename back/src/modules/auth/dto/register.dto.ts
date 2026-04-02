469bbabdaac21008dae199761a96a244fd1c1d31
Rogi Rocz <47047095+RogiRocz@users.noreply.github.com>
2026-04-02 03:37:15 +0000

    feat: criação de DTO para registrar um novo usuário

---
+++ back/src/modules/auth/dto/register.dto.ts
@@ -0,0 +1,28 @@
+import { ApiProperty, getSchemaPath } from "@nestjs/swagger";
+import { Type } from "class-transformer";
+import { IsEnum, ValidateNested } from "class-validator";
+import { UserType } from "src/common/interfaces/IAuth.interface";
+import { CreateAlunoDTO } from "src/modules/aluno/dto/create-aluno.dto";
+import { CreatePacienteDTO } from "src/modules/paciente/dto/create-paciente.dto";
+
+export class RegisterDTO {
+    @ApiProperty({ enum: UserType })
+    @IsEnum(UserType)
+    type: UserType;
+
+    @ApiProperty({
+        description: 'Dados específicos do usuário',
+        oneOf: [
+            { $ref: getSchemaPath(CreateAlunoDTO) },
+            { $ref: getSchemaPath(CreatePacienteDTO) },
+        ]
+    })
+    @ValidateNested()
+    @Type((opts) => {
+        const type = opts?.object?.type;
+        if (type === UserType.ALUNO) return CreateAlunoDTO;
+        if (type === UserType.PACIENTE) return CreatePacienteDTO;
+        return Object;
+    })
+    userData: CreateAlunoDTO | CreatePacienteDTO;
+}
