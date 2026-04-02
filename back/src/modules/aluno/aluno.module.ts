8e74b66979889401291fd8a4108a33b28ee97c2c
Rogi Rocz <47047095+RogiRocz@users.noreply.github.com>
2026-04-02 15:11:22 +0000

    feat: adicionando PrismaModule

--- back/src/modules/aluno/aluno.module.ts
+++ back/src/modules/aluno/aluno.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { AlunoService } from "./aluno.service";
 import { AlunoController } from "./aluno.controller";
+import { PrismaModule } from "src/common/Prisma/prisma.module";
 
 @Module({
+    imports: [PrismaModule],
     providers: [AlunoService],
     controllers: [AlunoController],
     exports: [AlunoService]
--- back/src/modules/paciente/paciente.module.ts
+++ back/src/modules/paciente/paciente.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { PacienteService } from "./paciente.service";
 import { PacienteController } from "./paciente.controller";
+import { PrismaModule } from "src/common/Prisma/prisma.module";
 
 @Module({
+    imports: [PrismaModule],
     providers: [PacienteService],
     controllers: [PacienteController],
     exports: [PacienteService]
