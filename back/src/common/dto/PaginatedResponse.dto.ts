be05cc98a49355fd2eb107ff00c929a5d75c0c8e
Rogi Rocz <47047095+RogiRocz@users.noreply.github.com>
2026-04-02 22:02:58 +0000

    feat: adicioanando descrição no swagger

--- back/src/common/dto/PaginatedResponse.dto.ts
+++ back/src/common/dto/PaginatedResponse.dto.ts
@@ -1,9 +1,30 @@
+import { ApiProperty } from "@nestjs/swagger";
+
+class PaginationMetadata {
+    @ApiProperty({description: 'Página atual'})
+    page: number;
+    @ApiProperty({description: 'Tamanho da página'})
+    size: number;
+    @ApiProperty({description: 'Total de páginas'})
+    totalPages: number;
+    @ApiProperty({description: 'Total de itens'})
+    totalItems: number;
+}
+
 export class PaginatedResponse<E> {
-    metadata: {
-        page: number,
-        size: number,
-        totalPages: number,
-        totalItems: number
-    };
+    @ApiProperty({description: 'Metadados sobre a paginação'})
+    metadata: PaginationMetadata;
+
+    @ApiProperty({
+        description: 'Lista de registros encontrados',
+        type: 'array',
+        items: {
+            oneOf: [
+                { $ref: 'Paciente' },
+                { $ref: 'Aluno' },
+                { $ref: 'Prontuario' },
+            ],
+        },
+    })
     data: E[];
 }
