36f58e2dbb9ae059fd93c2f345e871592ba0ec37
Rogi Rocz <47047095+RogiRocz@users.noreply.github.com>
2026-04-02 15:18:00 +0000

    feat: adicionando exemplos de inserção de dados e retornando o token

--- back/src/modules/auth/auth.controller.ts
+++ back/src/modules/auth/auth.controller.ts
@@ -1,23 +1,61 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { LoginDTO } from "./dto/login.dto";
-import { UserPayload } from "src/common/dto/UserPayload.dto";
 import { RegisterDTO } from "./dto/register.dto";
 import { Public } from "src/common/decorator/public.decorator";
+import { ApiExtraModels, ApiBody } from "@nestjs/swagger";
+import { CreateAlunoDTO } from "../aluno/dto/create-aluno.dto";
+import { CreatePacienteDTO } from "../paciente/dto/create-paciente.dto";
 
 @Public()
 @Controller('auth')
+@ApiExtraModels(CreateAlunoDTO, CreatePacienteDTO)
 export class AuthController {
-    constructor (private readonly auth: AuthService) {}
+    constructor(private readonly auth: AuthService) { }
 
     @Post('register')
-    async registerUser(@Body() dados: RegisterDTO) : Promise<UserPayload> {
+    @ApiBody({
+        type: RegisterDTO,
+        examples: {
+            aluno: {
+                summary: 'Exemplo Aluno',
+                description: 'Payload completo para cadastro de Aluno',
+                value: {
+                    type: 'aluno',
+                    userData: {
+                        matricula: '235689',
+                        nome: 'João Silva',
+                        email: 'aluno@example.com',
+                        senha: 'SenhaForte!123',
+                        periodo: 10,
+                        papel: 'ALUNO',
+                        ativo: true
+                    }
+                }
+            },
+            paciente: {
+                summary: 'Exemplo Paciente',
+                description: 'Payload completo para cadastro de Paciente',
+                value: {
+                    type: 'paciente',
+                    userData: {
+                        nome: 'Maria Souza',
+                        cpf: '12345678901',
+                        senha: 'SenhaForte!123',
+                        data_nascimento: '1995-05-20',
+                        prontuario_status: 'ATIVO'
+                    }
+                }
+            }
+        }
+    })
+    async registerUser(@Body() dados: RegisterDTO): Promise<string> {
         return this.auth.registerUser(dados.type, dados.userData)
     }
 
     @Post('login')
-    async loginUser(@Body() dados: LoginDTO): Promise<UserPayload>{
-        const {type, ...credenciais} = dados
+    async loginUser(@Body() dados: LoginDTO): Promise<string> {
+        const { type, ...credenciais } = dados
         return this.auth.loginUser(type, credenciais)
     }
 }
