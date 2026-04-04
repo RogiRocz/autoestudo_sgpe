a8726af66403d57053ef0f6ca2ee10c60a89e10d
Rogi Rocz <47047095+RogiRocz@users.noreply.github.com>
2026-04-04 00:42:54 +0000

    refat: movendo para shared

--- front/src/api/shared/config.ts
+++ front/src/api/shared/config.ts
@@ -1,6 +1,6 @@
 const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://localhost:3000';
 
-export async function apiFetch<T>(
+export async function apiFetch<T}>(
     endpoint: string,
     options: RequestInit = {}
 ): Promise<T> {
@@ -20,6 +20,9 @@ export async function apiFetch<T>(
 
     const response = await fetch(`${BASE_URL}${endpoint}`, config);
 
+    if (response.status === 204) {
+        return {} as T;
+    }
 
     const newToken = response.headers.get('Authorization');
     if (newToken && typeof window !== 'undefined') {
