7a0333c7193c84827bc25315bab963cdaac56bfb
Rogi Rocz <47047095+RogiRocz@users.noreply.github.com>
2026-04-04 00:46:34 +0000

    feat: função geradora dos metódos base das entidades

---
+++ front/src/api/shared/baseService.ts
@@ -0,0 +1,16 @@
+import { apiFetch } from "./config";
+
+export const createBaseService = <T, UpdateDTO>(routeName: string) => {
+    return {
+        update: async (id: string, data: UpdateDTO) =>
+            apiFetch<T>(`${routeName}/${id}`, {
+                method: 'PATCH',
+                body: JSON.stringify(data)
+            }),
+
+        deactivate: async (id: string) =>
+            apiFetch<void>(`${routeName}/${id}`, {
+                method: "DELETE"
+            }),
+    };
+};
