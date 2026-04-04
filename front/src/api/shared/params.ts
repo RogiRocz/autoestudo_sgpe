bfd3d90c90add4980ada33bedd7323e1f25a9175
Rogi Rocz <47047095+RogiRocz@users.noreply.github.com>
2026-04-04 00:48:01 +0000

    feat: função geradora dos parametros da url

---
+++ front/src/api/shared/params.ts
@@ -0,0 +1,30 @@
+import { QueryParams } from "@/types/shared/pagination"
+
+export const createParams = <T extends Record<string, any>>(params?: T & Partial<QueryParams>): string => {
+    const search = new URLSearchParams()
+
+    if(!params) return ""
+
+    const defaultParams: QueryParams = {
+        page: 1,
+        size: 10,
+        orderBy: 'criadoEm',
+        order: 'asc',
+    }
+
+    const merge = {...defaultParams, ...params}
+
+    Object.entries(merge).forEach(([key, value]) => {
+        if(value != undefined && value != null && value != ''){
+            if (value instanceof Date) {
+                search.append(key, value.toISOString());
+            } else {
+                search.append(key, String(value));
+            }
+        }
+    })
+
+    const finalParams = '?' + search.toString()
+
+    return finalParams
+}
