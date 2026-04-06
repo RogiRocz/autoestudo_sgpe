f4f6bb426c4aafbca1ec5306c167b10dcc66ffad
Rogi Rocz <47047095+RogiRocz@users.noreply.github.com>
2026-04-06 01:15:21 +0000

    feat: adicionando funções para alterar usuário e token separademente

--- front/src/store/auth.store.ts
+++ front/src/store/auth.store.ts
@@ -1,19 +1,23 @@
-import { AnyUser} from "@/types/shared/user.interface"
+import { AnyUser } from "@/types/shared/user.interface"
 import { create } from "zustand"
 import { persist } from "zustand/middleware"
 
-interface AuthStore {
+interface AuthState {
     token: string | null
     user: AnyUser | null
+    setToken: (newToken: string) => void
+    setUser: (newUser: AnyUser) => void
     setAuth: (token: string, user: AnyUser) => void
     logout: () => void
 }
 
-export const useAuthStore = create<AuthStore>()(
+export const useAuthStore = create<AuthState>()(
     persist(
         (set) => ({
             user: null,
             token: null,
+            setToken: (newToken: string) => set(() => ({ token: newToken })),
+            setUser: (newUser: AnyUser) => set(() => ({ user: newUser })),
             setAuth: (token, user) => set({ user, token }),
             logout: () => set({ user: null, token: null }),
         }),
