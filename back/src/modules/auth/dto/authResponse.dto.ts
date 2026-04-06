2360472f932b64031941ac7e7d6c54d24863fde2
Rogi Rocz <47047095+RogiRocz@users.noreply.github.com>
2026-04-06 01:14:31 +0000

    feat: DTO para o retorno da autenticação

---
+++ back/src/modules/auth/dto/authResponse.dto.ts
@@ -0,0 +1,25 @@
+import { UserType } from '@common/interfaces/IAuth.interface';
+import { AlunoViewDTO } from '@modules/aluno/dto/alunoView.dto';
+import { PacienteViewDTO } from '@modules/paciente/dto/pacienteView.dto';
+import { ApiProperty, getSchemaPath } from '@nestjs/swagger';
+
+export class AuthResponseDTO {
+    @ApiProperty({ description: 'token assinado com informações básicas sobre o usuário' })
+    token: string;
+
+    @ApiProperty({
+        description: 'Dados do usuário',
+        oneOf: [
+            { $ref: getSchemaPath(AlunoViewDTO) },
+            { $ref: getSchemaPath(PacienteViewDTO) },
+        ],
+        discriminator: {
+            propertyName: 'type',
+            mapping: {
+                [UserType.ALUNO]: getSchemaPath(AlunoViewDTO),
+                [UserType.PACIENTE]: getSchemaPath(PacienteViewDTO),
+            }
+        }
+    })
+    user: AlunoViewDTO | PacienteViewDTO;
+}
