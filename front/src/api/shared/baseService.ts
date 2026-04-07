d639a6cddf602d80c31cb41161a34e5c3a662d5e
Rogi Rocz <47047095+RogiRocz@users.noreply.github.com>
2026-04-07 12:17:02 +0000

    style: excluindo comentario

--- front/src/api/shared/baseService.ts
+++ front/src/api/shared/baseService.ts
@@ -2,8 +2,6 @@ import { apiFetch } from "./config";
 
 export const createBaseService = <T, UpdateDTO>(routeName: string) => {
     return {
-        // getMe: async () => apiFetch<T>(`${routeName}/${uuid zustand}`),
-
         update: async (id: string, data: UpdateDTO) =>
             apiFetch<T>(`${routeName}/${id}`, {
                 method: 'PATCH',
