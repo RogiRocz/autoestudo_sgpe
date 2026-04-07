c9faac03f8140feec3c116979cfb93a82ce155f7
Rogi Rocz <47047095+RogiRocz@users.noreply.github.com>
2026-04-07 23:21:39 +0000

    refat: ajuste para aplicar TIPO_USUARIO

--- front/src/types/shared/ownerData.ts
+++ front/src/types/shared/ownerData.ts
@@ -1,4 +1,11 @@
+import { TIPO_USUARIO } from "../enums/enums";
+
+export const MAP_TIPO_DONO = {
+    [TIPO_USUARIO.ALUNO]: 'aluno_id',
+    [TIPO_USUARIO.PACIENTE]: 'paciente_id',
+} as const
+
 export interface OwnerData {
-    tipoDono: 'aluno_id' | 'paciente_id';
+    tipoDono: typeof MAP_TIPO_DONO[TIPO_USUARIO],
     donoId: string;
 }
