708ab2393eb993262760b3894deb80ba964052a9
Rogi Rocz <rogirocz@gmail.com>
2026-04-11 17:44:21 -0300

    feat: adicionando criação de usuário em cache pelo react query

---
+++ front/src/hooks/mutations/useAuthMutations.mutation.ts
@@ -0,0 +1,12 @@
+import { useMutation } from "@tanstack/react-query"
+import { AuthService } from "../../api/auth/auth.api"
+import { RegisterUser } from "@/types/auth/auth"
+
+export const useRegisterUser = () => {
+    return useMutation({
+        mutationKey: ['users', 'create'],
+        mutationFn: async ({ type, userData }: RegisterUser) => {
+            return await AuthService.register({type, userData})
+        }
+    })
+}
