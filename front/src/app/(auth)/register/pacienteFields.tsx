b47e49197b24ad8280f64480aabcec820f5bb1bc
Rogi Rocz <rogirocz@gmail.com>
2026-04-11 17:42:33 -0300

    feat: adicionando lógica de register e control e ajeitando uns campos

--- front/src/app/(auth)/register/pacienteFields.tsx
+++ front/src/app/(auth)/register/pacienteFields.tsx
@@ -1,26 +1,29 @@
-'use client'
+"use client"
 
 import { Field, FieldGroup, FieldLabel } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
+import { FormValues } from "@/utils/schemas.validator"
+import { Control, UseFormRegister } from "react-hook-form"
 
-export function PacienteFields() {
-    // nome: string
-    // cpf: string
-    // data_nascimento: Date;
-    return (
-      <FieldGroup>
-        <Field>
-          <FieldLabel htmlFor="name">Nome</FieldLabel>
-          <Input id="name" type="text"></Input>
-        </Field>
-        <Field>
-          <FieldLabel htmlFor="cpf">CPF</FieldLabel>
-          <Input id="cpf" type="text"></Input>
-        </Field>
-        <Field>
-          <FieldLabel htmlFor="birth">Data de Nascimento</FieldLabel>
-          <Input id="birth" type="text"></Input>
-        </Field>
-      </FieldGroup>
-    )
+export function PacienteFields({register, control} : {register: UseFormRegister<FormValues>, control?: Control}) {
+  return (
+    <FieldGroup>
+      <Field>
+        <FieldLabel htmlFor="nome">Nome</FieldLabel>
+        <Input id="nome" type="text" {...register('nome')}></Input>
+      </Field>
+      <Field>
+        <FieldLabel htmlFor="cpf">CPF</FieldLabel>
+        <Input id="cpf" type="text" {...register('cpf')}></Input>
+      </Field>
+      <Field>
+        <FieldLabel htmlFor="senha">Senha</FieldLabel>
+        <Input id="senha" type="text" {...register('senha')}></Input>
+      </Field>
+      <Field>
+        <FieldLabel htmlFor="data_nascimento">Data de Nascimento</FieldLabel>
+        <Input id="data_nascimento" type="text" {...register('data_nascimento')}></Input>
+      </Field>
+    </FieldGroup>
+  )
 }
