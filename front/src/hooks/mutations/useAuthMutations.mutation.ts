ec9a13ab936773847510e7dfe83d16e205aa2126
Samuel Costa <samuelcosta.pd@gmail.com>
2026-04-28 15:51:46 -0300

    feat: criando query para login do usuário

--- front/src/hooks/mutations/useAuthMutations.mutation.ts
+++ front/src/hooks/mutations/useAuthMutations.mutation.ts
@@ -1,12 +1,21 @@
 import { useMutation } from "@tanstack/react-query"
 import { AuthService } from "../../api/auth/auth.api"
-import { RegisterUser } from "@/types/auth/auth"
+import { LoginRequest, RegisterUser } from "@/types/auth/auth"
 
 export const useRegisterUser = () => {
   return useMutation({
-    mutationKey: ["users", "create"],
+    mutationKey: ["users", "register"],
     mutationFn: async ({ type, userData }: RegisterUser) => {
       return await AuthService.register({ type, userData })
+    },
+  })
+}
+
+export const useLoginUser = () => {
+  return useMutation({
+    mutationKey: ["users", "login"],
+    mutationFn: async ({ login, senha, field, type }: LoginRequest) => {
+      return await AuthService.login({ login, senha, field, type })
     },
   })
 }
