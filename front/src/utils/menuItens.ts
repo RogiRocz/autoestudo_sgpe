4070f62220c60c227d3dd20a01b306d32dfc532b
RogiRocz <rogirocz@gmail.com>
2026-04-28 00:25:55 -0300

    fix: ajustando para retorna já o

---
+++ front/src/utils/menuItens.ts
@@ -0,0 +1,17 @@
+import HomeIcon from '@mui/icons-material/Home';
+import RecentActorsIcon from '@mui/icons-material/RecentActors';
+import PersonIcon from '@mui/icons-material/Person';
+import { OverridableComponent } from '@mui/material/OverridableComponent';
+import { SvgIconTypeMap } from '@mui/material/SvgIcon';
+
+interface IMENU_ITENS {
+	label: string,
+	href: string
+	icon: OverridableComponent<SvgIconTypeMap<{}, "svg">>
+}
+
+export const MENU_ITENS = <IMENU_ITENS[]>[
+	{ label: 'Home', href: '/', icon: HomeIcon },
+	{ label: 'Prontuarios', href: '/prontuarios', icon: RecentActorsIcon },
+	{ label: 'Perfil', href: '/profile', icon: PersonIcon },
+]
