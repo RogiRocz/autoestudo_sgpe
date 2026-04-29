2551cce78673e7bb80f1c4206ee656018503c086
RogiRocz <rogirocz@gmail.com>
2026-04-28 22:11:53 -0300

    refat: mudando para portugês brasileiro

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
 }>) {
     return (
         <html
-            lang="en"
+            lang="pt_br"
             suppressHydrationWarning
             className={cn(
                 'antialiased',
