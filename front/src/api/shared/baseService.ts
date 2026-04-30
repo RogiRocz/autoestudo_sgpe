e79622dec0c7d52356d5258011c9b8e7c00644f9
RogiRocz <rogirocz@gmail.com>
2026-04-30 11:28:10 -0300

    feat: criando metodo pra retornar todos paginados

--- front/src/api/shared/baseService.ts
+++ front/src/api/shared/baseService.ts
@@ -1,16 +1,26 @@
+import { PaginationResponse, QueryParams } from '@/types/shared/pagination'
 import { apiFetch } from './config'
+import { createParams } from './params'
 
 export const createBaseService = <T, UpdateDTO>(routeName: string) => {
-    return {
-        update: async (id: string, data: UpdateDTO) =>
-            apiFetch<T>(`${routeName}/${id}`, {
-                method: 'PATCH',
-                body: JSON.stringify(data),
-            }),
+	return {
+		findAllPaginated: async (params: QueryParams) => {
+			const formattedParams = createParams(params)
+			return apiFetch<PaginationResponse<T>>(`${routeName}/${formattedParams}`, {
+				method: 'GET',
 
-        deactivate: async (id: string) =>
-            apiFetch<void>(`${routeName}/desativar/${id}`, {
-                method: 'PATCH',
-            }),
-    }
+			})
+		},
+
+		update: async (id: string, data: UpdateDTO) =>
+			apiFetch<T>(`${routeName}/${id}`, {
+				method: 'PATCH',
+				body: JSON.stringify(data),
+			}),
+
+		deactivate: async (id: string) =>
+			apiFetch<void>(`${routeName}/desativar/${id}`, {
+				method: 'PATCH',
+			}),
+	}
 }
