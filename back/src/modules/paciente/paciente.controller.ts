f31b9043ec915389a02aa28d5f87c05ad406a552
RogiRocz <rogirocz@gmail.com>
2026-04-30 11:27:45 -0300

    fix: ajustando para pegar do parametro

--- back/src/modules/paciente/paciente.controller.ts
+++ back/src/modules/paciente/paciente.controller.ts
@@ -1,13 +1,13 @@
 import {
-    Body,
-    Controller,
-    Delete,
-    Get,
-    HttpCode,
-    Param,
-    Patch,
-    Post,
-    Query,
+	Body,
+	Controller,
+	Delete,
+	Get,
+	HttpCode,
+	Param,
+	Patch,
+	Post,
+	Query,
 } from '@nestjs/common'
 import { PacienteService } from './paciente.service'
 import { Paciente } from './entites/paciente.entity'
@@ -21,49 +21,49 @@ import { ApiPaginatedResponse } from '@common/decorator/paginated.decorator'
 @ApiTags('pacientes')
 @Controller('pacientes')
 export class PacienteController {
-    constructor(private pacienteService: PacienteService) {}
+	constructor(private pacienteService: PacienteService) { }
 
-    @Post()
-    async createPaciente(
-        @Body() dadosPaciente: CreatePacienteDTO
-    ): Promise<Paciente> {
-        return await this.pacienteService.create(dadosPaciente)
-    }
+	@Post()
+	async createPaciente(
+		@Body() dadosPaciente: CreatePacienteDTO
+	): Promise<Paciente> {
+		return await this.pacienteService.create(dadosPaciente)
+	}
 
-	@Get('search')
-	async searchPacientes(word: string): Promise<Paciente[]> {
+	@Get('search/:word')
+	async searchPacientes(@Param('word') word: string): Promise<Paciente[]> {
 		return await this.pacienteService.search(word)
 	}
 
-    @Get()
-    @ApiPaginatedResponse(Paciente)
-    async findAllPacientes(
-        @Query() params: QueryParamsDTO
-    ): Promise<PaginatedResponse<Paciente>> {
-        return await this.pacienteService.findAll(params)
-    }
+	@Get()
+	@ApiPaginatedResponse(Paciente)
+	async findAllPacientes(
+		@Query() params: QueryParamsDTO
+	): Promise<PaginatedResponse<Paciente>> {
+		return await this.pacienteService.findAll(params)
+	}
 
-    @Get(':id')
-    async findPacienteById(@Param('id') id: string): Promise<Paciente> {
-        return this.pacienteService.findById(id)
-    }
+	@Get(':id')
+	async findPacienteById(@Param('id') id: string): Promise<Paciente> {
+		return this.pacienteService.findById(id)
+	}
 
-    @Patch(':id')
-    async updatePaciente(
-        @Param('id') id: string,
-        @Body() dadosNovos: UpdatePacienteDTO
-    ): Promise<Paciente> {
-        return this.pacienteService.update(id, dadosNovos)
-    }
+	@Patch(':id')
+	async updatePaciente(
+		@Param('id') id: string,
+		@Body() dadosNovos: UpdatePacienteDTO
+	): Promise<Paciente> {
+		return this.pacienteService.update(id, dadosNovos)
+	}
 
-    @Patch('/desativar/:id')
-    @HttpCode(204)
-    async deactivate(@Param('id') id: string): Promise<void> {
-        await this.pacienteService.deactivate(id)
-    }
+	@Patch('/desativar/:id')
+	@HttpCode(204)
+	async deactivate(@Param('id') id: string): Promise<void> {
+		await this.pacienteService.deactivate(id)
+	}
 
-    @Delete(':id')
-    async deletePaciente(@Param('id') id: string): Promise<object> {
-        return this.pacienteService.delete(id)
-    }
+	@Delete(':id')
+	async deletePaciente(@Param('id') id: string): Promise<object> {
+		return this.pacienteService.delete(id)
+	}
 }
