aaae3013010126e940a7a89b0fffb370ef0b11cb
RogiRocz <rogirocz@gmail.com>
2026-05-01 13:27:07 -0300

    feat: renomeando e adicionando propriedades

--- back/src/modules/config/config.service.ts
+++ back/src/modules/config/config.service.ts
@@ -1,15 +1,17 @@
+import { UserType } from '@common/interfaces/IAuth.interface';
 import { Injectable } from '@nestjs/common';
 import { PAPEIS, CLIENTE_PRONTUARIO_STATUS, TIPO_SESSAO, LOCAL_SESSAO, PRONTUARIO_STATUS } from '@prisma/client';
 
 @Injectable()
 export class ConfigService {
-    getEnums() {
-        return {
-            papeis: Object.values(PAPEIS),
-            statusProntuario: Object.values(CLIENTE_PRONTUARIO_STATUS),
-            tiposSessao: Object.values(TIPO_SESSAO),
-            local_sessao: Object.values(LOCAL_SESSAO),
-            prontuario_status: Object.values(PRONTUARIO_STATUS)
-        };
-    }
+	getEnums() {
+		return {
+			papeis: Object.values(PAPEIS),
+			statusProntuario: Object.values(CLIENTE_PRONTUARIO_STATUS),
+			tiposSessao: Object.values(TIPO_SESSAO),
+			locaisSessao: Object.values(LOCAL_SESSAO),
+			prontuarioStatus: Object.values(PRONTUARIO_STATUS),
+			tiposUsuario: Object.values(UserType)
+		};
+	}
 }
