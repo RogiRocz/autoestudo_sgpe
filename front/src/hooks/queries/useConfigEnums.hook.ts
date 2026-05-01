4665dccaa909a846af9b4630823eab681b106e8a
RogiRocz <rogirocz@gmail.com>
2026-05-01 13:28:40 -0300

    feat: query para pegar as config do back

---
+++ front/src/hooks/queries/useConfigEnums.hook.ts
@@ -0,0 +1,13 @@
+import { ConfigService } from "@/api/config/config.api"
+import { useQuery } from "@tanstack/react-query"
+
+const tableKey = 'config'
+
+export const useConfigEnums = () => {
+	return useQuery({
+		queryKey: [tableKey, 'getAll'],
+		queryFn: async () => {
+			return ConfigService.getEnums()
+		}
+	})
+}
