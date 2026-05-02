990cfa4f8aebf97c141a06d28bc1f46b82d97224
RogiRocz <rogirocz@gmail.com>
2026-05-02 11:22:18 -0300

    feat: criando rota pra procurar aluno por nome

--- front/src/api/aluno/aluno.api.ts
+++ front/src/api/aluno/aluno.api.ts
@@ -1,11 +1,18 @@
 import { Aluno } from '@/types/aluno/aluno.interface'
 import { UpdateAlunoDTO } from '@/types/aluno/aluno.dto'
 import { createBaseService } from '../shared/baseService'
+import { apiFetch } from '../shared/config'
 
 const routeName = 'alunos'
 
 const baseMethods = createBaseService<Aluno, UpdateAlunoDTO>(routeName)
 
 export const AlunoService = {
-    ...baseMethods,
+	...baseMethods,
+	search: async (word: string): Promise<Aluno[]> => {
+		const params = new URLSearchParams({ word })
+		return apiFetch(`${routeName}/search?${params.toString()}`, {
+			method: 'GET',
+		})
+	},
 }
