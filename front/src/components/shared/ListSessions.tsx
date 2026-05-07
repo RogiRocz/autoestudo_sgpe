61aa30c7bdb5ba7fa265df8f081842a6c6c8a06c
Samuel Costa <samuelcosta.pd@gmail.com>
2026-05-07 15:34:29 -0300

    feat: estrutura inicial pra exibir os cards

--- front/src/components/shared/ListSessions.tsx
+++ front/src/components/shared/ListSessions.tsx
@@ -1,7 +1,22 @@
 'use client'
 
-export function ListSessions() {
-    
+import { useAuthUser } from '@/hooks/queries/useAuthUser.hook'
+import { prontuariosStore } from '@/store/prontuario.store'
+import { Col, Container } from '../ui/grid'
+import { CardSesison } from './CardSession'
 
-    return (<></>)
+export function ListSessions() {
+    const { prontuarios } = prontuariosStore()
+
+    return (
+        <Container>
+            <Col>
+                {prontuarios?.map((p, i) => {
+                    return (
+                        <CardSesison key={i} {...p} title={`Sessão ${i + 1}`} />
+                    )
+                })}
+            </Col>
+        </Container>
+    )
 }
